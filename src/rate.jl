8071de0422315f717c39170aeeb56c54f157bc3d
biplab37 <biplabmahato37@gmail.com>
2023-12-05 12:41:44 +0100

    added production rate and density

--- src/rate.jl
+++ src/rate.jl
@@ -9,3 +9,10 @@ end
 function calc_region(amp, pulse::Pulse)
     return amp * [-1.0 1.0; -1.0 1.0]
 end
+
+function prod_rate(amp_range::AbstractVector, pulse::Pulse)
+    rates = zeros(length(amp_range))
+    for (i, amp) in enumerate(amp_range)
+        rates[i] = prod_rate(amp, pulse)
+    end
+end
--- src/solution.jl
+++ src/solution.jl
@@ -69,4 +69,24 @@ function fsection_y(kxrange, ky, p::Pulse)
     return dist_f
 end
 
-export f_atk, distribution_f, fsection_x, fsection_y, distribution_list
+function f_atk_func(k1, k2, p::Pulse, initial_value=[0.0, 0.0, 0.0])
+    if k1 == 0 && k2 == 0
+        return initial_value
+    end
+    prob = ODEProblem(odes!, initial_value, (p.start, p.finish), [[k1, k2], p])
+    return solve(prob, Tsit5())
+end
+
+function density(t, p::Pulse; initial_value=[0.0, 0.0, 0.0], region=p.Amp * [-1.0 1.0; -1.0 1.0])
+    return quadgk((kx, ky) -> f_atk_func(kx, ky, p, initial_value)(t), eachcol(region)[1], eachcol(region)[2])[1]
+end
+
+function density(trange::AbstractVector, p::Pulse; initial_value=[0.0, 0.0, 0.0], region=p.Amp * [-1.0 1.0; -1.0 1.0])
+    d_list = zeros(trange)
+    for (i, t) in enumerate(trange)
+        d_list[i] = density(t, p, initial_value=initial_value, region=region)
+    end
+    return d_list
+end
+
+export f_atk, distribution_f, fsection_x, fsection_y, distribution_list, density
